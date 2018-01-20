cd12ef342c67212651b76d0d3de018407ca3e4ce
LucasTaboada <ltlucastaboada@gmail.com>
2018-01-20 00:53:49 -0500

    add views and public

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+app = express(),
+pug = require("pug"),
+bodyParser = require("body-parser");
+
+app.set("view engine","pug");
+app.use(express.static("public"));
+
+
+
+app.get("/", function(req,res){
+    res.render("index");
+});
+
+
+
+app.listen(3000, function(){
+    console.log("app running on port:3000");
+})
